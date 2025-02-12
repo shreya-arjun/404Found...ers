b5c3f4149f926ec5caca0ece4833133e6ddb4192
mallworth <maxallworthmiles@gmail.com>
2025-02-11 16:09:23 -0800

    Prototype seed generation

---
+++ packages/backend/services/suggestionService.js
@@ -0,0 +1,10 @@
+/**
+ * Gets track suggestions from Spotify Web API
+ * @param {JSON} seed - Target values for danceability, energy, speechiness, and valence
+ */
+export default function getSuggestions(seed) {
+    // Get users top artists and tracks and add to seed
+    // with names seed_artists, seed_tracks
+
+    // Get track suggestions with updated seed
+}
