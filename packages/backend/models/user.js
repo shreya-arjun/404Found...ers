c3b9f17fedcdedba8192bac77650e4bcd34a3762
Shreya  Arjun <shreyaarjun@Shreyas-MacBook-Air.local>
2025-02-14 09:18:26 -0800

    Initial commit for new branch

---
+++ packages/backend/models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const SuggestionSchema = new mongoose.Schema({
+    mood: {
+        type: String, 
+        required: true 
+    },
+    track: {
+        type: String, 
+        required: true 
+    } // Assuming track is a Spotify track ID
+}); 
+
+const UserSchema = new mongoose.Schema({
+    spotifyId: {
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    suggestions: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Suggestion' 
+    }]
+});
+
+const User = mongoose.model('User', UserSchema);
+const Suggestion = mongoose.model('Suggestion', SuggestionSchema);
+
+module.exports = { User, Suggestion };
+
