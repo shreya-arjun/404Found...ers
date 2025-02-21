1cc6196531e4cbe3572a792d68d753f0da7da60c
Ella Yam <ellalyam@gmail.com>
2025-02-21 09:42:05 -0800

    Starting emotion recgonition

--- packages/frontend/src/services/emotionRecognitionService.ts
+++ packages/frontend/src/services/emotionRecognitionService.ts
@@ -1,3 +1,5 @@
+import { HumeClient } from "hume";
+
 class EmotionRecognitionService {
     // Receives images and sends to Hume for analysis
     public static async identifyEmotion(imageSrc: string): Promise<void> {
@@ -5,9 +7,18 @@ class EmotionRecognitionService {
             console.log("Processing image");
 
             // Send to Hume.ai
+            //const client = new HumeClient({ apiKey: "NyEnSqsDCJWluAYaBquATgHslcPB8Y0HC5T7mkfN0JiUp0SR" });
+            /*await client.expressionMeasurement.batch.startInferenceJob({
+                urls: [imageSrc],
+                notify: true
+            });*/
 
-            // Get response & send to backend
+            // Get response
+            // await client.expressionMeasurement.batch.getJobPredictions("job_id");
+            
 
+            // Send to backend
+            
         } catch (error) {
             console.error("Error processing image");
         }
