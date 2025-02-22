bdcaa72a401c34cc2b685c47d87bbdfc85680fe9
Gwendolyn Wren <137141797+0x4777656e@users.noreply.github.com>
2025-02-21 17:45:04 -0800

    add function to refresh expired token

--- packages/frontend/src/services/spotifyLoginService.ts
+++ packages/frontend/src/services/spotifyLoginService.ts
@@ -70,7 +70,28 @@ export class SpotifyLoginService {
   }
 
   public static async refreshAccessToken() {
-    // TODO
+    const refreshToken = localStorage.getItem("spotify_refresh_token");
+    const url = "https://accounts.spotify.com/api/token";
+
+    const payload = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+        client_id: clientId,
+      }),
+    }
+
+    const body = await fetch(url, payload);
+    const response = await body.json();
+
+    localStorage.setItem("spotify_access_token", response.accessToken);
+    if (response.refreshToken) {
+      localStorage.setItem("spotify_refresh_token", response.refreshToken);
+    }
   }
 
   private static generateRandomStr(length: number): string {
